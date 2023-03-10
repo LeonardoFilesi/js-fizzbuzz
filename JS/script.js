ef6283995ef1203eba76487db3084ca6594425dc
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-03-10 16:02:46 +0100

    feat: :sparkles: Adds finished exercise, it may be ugly, but it's good

--- JS/script.js
+++ JS/script.js
@@ -5,21 +5,23 @@ for (let i = 1; i <= 50; i++) {
     let classColor;
 
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        /* console.log("FizzBuzz"); */
         oddEven = "divisibilex15";
         classColor = "fizzbuzz";
     } else if (i % 5 === 0) {
-        console.log("Buzz");
+        /* console.log("Buzz"); */
         oddEven = "divisibilex5";
         classColor = "buzz";
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+        /* console.log("Fizz"); */
         oddEven = "divisibilex3";
         classColor = "fizz";
     } else {
-        console.log(i);
+      /*   console.log(i); */
+        oddEven = "numerox";
+        classColor = i;
     }
 
-    console.log(i, oddEven);
-    gridElem.innerHTML += `<div class="box ${classColor}">${i}</div>`;
+    console.log(i, oddEven); 
+    gridElem.innerHTML += `<div class="box ${classColor}">${classColor}</div>`;
 }
